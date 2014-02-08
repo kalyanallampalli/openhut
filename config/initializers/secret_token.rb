2f3f37533697bbb56a5a1ad04b1d0163df739a40
Kalyan Allampalli <kalyan.allampalli@gmail.com>
2014-02-09 00:11:35 +0530

    Initial commit of Rails4 application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Openhut::Application.config.secret_key_base = 'f5de79c836d347fb90136bad91c19d16ebafaac7ff5c6d69c72e480b716ace2d696b374c9ce14bc42e8942da8de1f524bb4bf0601891caf7c2556c469689e8f7'
